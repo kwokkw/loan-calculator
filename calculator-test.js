5ea884b87d7a9f8ccf25de09bedd6cad005f1375
Kwok <kwokkw@users.noreply.github.com>
2024-03-25 17:47:00 -1000

    final commit

--- calculator-test.js
+++ calculator-test.js
@@ -4,15 +4,12 @@ describe("calculation", () => {
   });
 
   it("should return a result with 2 decimal places", function () {
-    expect(
-      calculateMonthlyPayment(
-        (loanObj = {
-          amount: 200000,
-          years: 30,
-          rate: 3,
-        })
-      )
-    ).toBeCloseTo(500.88, 2);
+    const loanObj = {
+      amount: 200000,
+      years: 30,
+      rate: 3,
+    };
+    expect(calculateMonthlyPayment(loanObj)).toEqual("499.59");
   });
 
   /// etc
